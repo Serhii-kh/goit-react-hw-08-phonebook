dfe42d4afb0df3bca27c39247f8a61d86ca70c0f
Serhii Naboka <drifterzzz@gmail.com>
2023-06-21 16:36:29 +0300

    upd thunks

--- src/redux/ContactsListSlice.js
+++ src/redux/ContactsListSlice.js
@@ -1,16 +1,12 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { initialState } from './initialState';
 import { CONTACTS } from './constants';
-import {
-  fetchContacts,
-  postContact,
-  deleteContactById,
-} from 'components/api';
+import { fetchContacts, postContact, deleteContactById } from 'components/api';
 
 const fetchContactsFulfilled = (state, { payload }) => {
-  state.items.isLoading = false;
   state.items = payload;
 };
+
 const postContactFulfilled = (state, { payload }) => {
   state.items.push(payload);
 };
@@ -19,6 +15,7 @@ const deleteContactFulfilled = (state, { payload }) => {
   const index = state.items.findIndex(contact => contact.id === payload.id);
   state.items.splice(index, 1);
 };
+
 const handlePending = state => {
   state.isLoading = true;
 };
@@ -27,55 +24,23 @@ const handleRejected = (state, { payload }) => {
   state.error = payload;
 };
 
+const handleFulfilled = state => {
+  state.error = null;
+  state.isLoading = false;
+};
+
 export const ContactsListSlice = createSlice({
   name: CONTACTS,
   initialState,
 
   extraReducers: builder => {
     builder
-      .addCase(fetchContacts.fulfilled, fetchContactsFulfilled)
-      .addCase(postContact.fulfilled, postContactFulfilled)
-      .addCase(deleteContactById.fulfilled, deleteContactFulfilled)
+      .addCase([fetchContacts.fulfilled], fetchContactsFulfilled)
+      .addCase([postContact.fulfilled], postContactFulfilled)
+      .addCase([deleteContactById.fulfilled], deleteContactFulfilled)
       .addMatcher(({ type }) => type.endsWith('/pending'), handlePending)
-      .addMatcher(({ type }) => type.endsWith('/rejected'), handleRejected);
-
-    // [fetchContacts.pending](state) {
-    //   state.isLoading = true;
-    // },
-    // [fetchContacts.fulfilled](state, { payload }) {
-    //   state.isLoading = false;
-    //   state.error = null;
-    //   state.items = payload;
-    // },
-    // [fetchContacts.rejected](state, { payload }) {
-    //   state.isLoading = false;
-    //   state.error = payload;
-    // },
-    // [postContact.pending](state) {
-    //   state.isLoading = true;
-    // },
-    // [postContact.fulfilled](state, { payload }) {
-    //   state.isLoading = false;
-    //   state.error = null;
-    //   state.items.push(payload);
-    // },
-    // [postContact.rejected](state, { payload }) {
-    //   state.isLoading = false;
-    //   state.error = payload;
-    // },
-    // [deleteContactById.pending](state) {
-    //   state.isLoading = true;
-    // },
-    // [deleteContactById.fulfilled](state, { payload }) {
-    //   state.isLoading = false;
-    //   state.error = null;
-    //   const index = state.items.findIndex(contact => contact.id === payload.id);
-    //   state.items.splice(index, 1);
-    // },
-    // [deleteContactById.rejected](state, { payload }) {
-    //   state.isLoading = false;
-    //   state.error = payload;
-    // },
+      .addMatcher(({ type }) => type.endsWith('/rejected'), handleRejected)
+      .addMatcher(({ type }) => type.endsWith('/fulfilled'), handleFulfilled);
   },
 });
 
