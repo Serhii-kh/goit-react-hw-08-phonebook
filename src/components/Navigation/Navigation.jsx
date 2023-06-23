47cb97ff6b28a26cc049e1b3f8e3c427126e13db
Serhii Naboka <drifterzzz@gmail.com>
2023-06-23 17:51:12 +0300

    dsvdvv

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,8 +1,10 @@
 import { NavLink } from "react-router-dom";
 import css from '../Navigation/Navigation.module.css'
+import { useDispatch } from "react-redux";
 
 export const Navigation = () => {
 	const setActive = ({ isActive }) => isActive ? 'active-link' : ''
+const dispatch = useDispatch()
 
 	return (
 		<>
@@ -11,6 +13,7 @@ export const Navigation = () => {
 					<NavLink to="/" className={setActive}>Register</NavLink>
 					<NavLink to="login" className={setActive}>Login</NavLink>
 					<NavLink to="contacts" className={setActive}>Contacts</NavLink>
+					<button type="submit" onClick={()=>{dispatch()}}>Logout</button>
 				</nav>
 			</header>
 		</>
