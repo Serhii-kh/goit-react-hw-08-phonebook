2ac7c85945ca02414238feb8f2676548099956c3
Serhii Naboka <drifterzzz@gmail.com>
2023-06-25 16:37:35 +0300

    delete wrapper

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,17 +1,14 @@
 import { Navigation } from "components/Navigation/Navigation"
-import { Wrapper } from "components/Wrapper/Wrapper"
 import { Suspense } from "react"
 import { Outlet } from "react-router-dom"
 
 const Layout = () => {
 	return (
 		<>
-			<Wrapper>
 				<Navigation />
 				<Suspense fallback={<div>Loading...</div>}>
 					<Outlet />
 				</Suspense>
-			</Wrapper>
 		</>
 	)
 }
--- src/components/wrapper/Wrapper.jsx
+++
@@ -1,8 +0,0 @@
-import css from './Wrapper.module.css'
-
-export const Wrapper = ({ children }) => {
-	return (
-		<div className={css.Wrapper}>
-			{children}
-		</div>)
-}
