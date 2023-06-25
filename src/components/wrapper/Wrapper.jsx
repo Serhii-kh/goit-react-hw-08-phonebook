5d299ab2071bf050cabc8c438e6a33b022882051
Serhii Naboka <drifterzzz@gmail.com>
2023-06-25 16:35:31 +0300

    add body styles

--- src/components/wrapper/Wrapper.jsx
+++ src/components/wrapper/Wrapper.jsx
@@ -1,10 +1,8 @@
 import css from './Wrapper.module.css'
-// import PropTypes from 'prop-types'
 
-export const Wrapper = ({ children }) => <div className={css.Wrapper}>{children}</div>
-
-// Wrapper.propTypes = {
-// 	children: PropTypes.node,
-// }
-
-
+export const Wrapper = ({ children }) => {
+	return (
+		<div className={css.Wrapper}>
+			{children}
+		</div>)
+}
